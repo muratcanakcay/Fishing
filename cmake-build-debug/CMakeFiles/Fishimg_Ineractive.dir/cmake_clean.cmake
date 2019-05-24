d861a4b861ff6de978c64a6aa2ddcac7fbcb1b7c
Suliaman Abass <suliaman.abass46@gmail.com>
2019-05-24 17:47:47 +0200

    no message

---
+++ cmake-build-debug/CMakeFiles/Fishimg_Ineractive.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fishimg_Ineractive.dir/destination_legality_check.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/floe_availability_check.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/get_destination_coordinates.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/get_penguin_coordinates.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/get_placement_coordinates.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/main.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/map_generator.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/move_penguins.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/movement_possibility_check.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/place_penguins.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/placement_legality_check.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/player_generator.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/print_map.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/update_map.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/placement_score_check.c.obj"
+  "CMakeFiles/Fishimg_Ineractive.dir/opening_score_check.c.obj"
+  "Fishimg_Ineractive.pdb"
+  "Fishimg_Ineractive.exe"
+  "Fishimg_Ineractive.exe.manifest"
+  "libFishimg_Ineractive.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Fishimg_Ineractive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
